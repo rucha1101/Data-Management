973f2bd250197953a72baa4d007387d30429ca5d
Rucha  Khartadkar <41960703+rucha1101@users.noreply.github.com>
2024-03-15 16:19:07 +0000

    Create test_load.R

---
+++ R/test_load.R
@@ -0,0 +1,13 @@
+
+
+
+
+my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"database.db")
+
+ this_filepath <- ("data_upload/Customers.csv") 
+  
+  this_file_contents <- read_csv(this_filepath) 
+  
+  table_name <- "Customers" 
+  
+dbWriteTable(my_connection, table_name, this_file_contents, row.names = FALSE, append = TRUE)
