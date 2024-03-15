04c4ffaa63feaf41aae46f3d72c74223a97f2687
rucha1101 <rucha.khartadkar123@gmail.com>
2024-03-15 11:32:38 +0000

    nw file

---
+++ R/data_load.R
@@ -0,0 +1,15 @@
+library(readr)
+library(RSQLite)
+
+# Load the datasets
+customer <- readr::read_csv("dataset/customer_v2.csv")
+
+# Connect to the database
+my_connection <- RSQLite::dbConnect(RSQLite::SQLite(), "database/group_33.db")
+
+# Append each data frame to its respective table in the database
+# Adjust the table names as needed
+RSQLite::dbWriteTable(my_connection, "customer", customer, append = TRUE)
+
+# Close the database connection
+RSQLite::dbDisconnect(my_connection)
