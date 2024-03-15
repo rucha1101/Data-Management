daeb5390a88506284841eb79ccb4938513ec0a6e
rucha1101 <rucha.khartadkar123@gmail.com>
2024-03-15 19:39:56 +0000

    added tables and updated data_load file

--- R/data_load.R
+++ R/data_load.R
@@ -39,7 +39,76 @@ RSQLite::dbExecute(my_connection,"
     'material' text  
     );
 ")
+RSQLite::dbExecute(my_connection,"
+create table shipment (
+    'shipment_id' text primary key,
+    'transaction_id' text not null,
+    'shipment_date' date not null,
+    'shipment_phone_number' text not null,
+    'shipment_address' text not null,
+    'shipment_city' text not null,
+    'shipment_state' text,
+    'shipment_zip_code' text,
+    'shipment_country' text,
+    foreign key (transaction_id) references transaction_detail(transaction_id)
+);
+")
 
+RSQLite::dbExecute(my_connection,"
+create table suppliers (
+    'supplier_id' text primary key,
+    'product_id' text not null,
+    'company_name' text,
+    'contact_name' text,
+    'supplier_address' text,
+    'supplier_phone_number' text,
+    'supplier_email' text,
+    'website' text,
+    foreign key (product_id) references products(product_id)
+);
+")
+
+RSQLite::dbExecute(my_connection,"
+create table categories (
+    'category_id' text primary key,
+    'category_name' text,
+    'category_description' text,
+    'parent_category_id' text,
+	'product_id' text not null,
+    foreign key (parent_category_id) references categories(category_id)
+	foreign key (product_id) references products(product_id)
+);
+")
+
+RSQLite::dbExecute(my_connection,"
+create table reviews (
+    'review_id' text primary key,
+    'product_id' text not null,
+    'customer_id' text not null,
+    'rating' integer,
+    'review_text' text,
+    'review_date' date,
+    foreign key (product_id) references products(product_id),
+    foreign key (customer_id) references customers(customer_id)
+);
+")
+
+RSQLite::dbExecute(my_connection,"
+create table transaction_details (
+    'transaction_id' text primary key,
+    'customer_id' text not null,
+    'product_id' text not null,
+    'quantity' integer,
+    'discount' real,
+    'total_price' real,
+	'transaction_date' date,
+    'payment_method' text,
+    'currency' text,
+    'payment_processor' text,
+    foreign key (product_id) references products(product_id),
+    foreign key (customer_id) references customers(customer_id)
+);
+")
 
 dbDisconnect(my_connection)
 
