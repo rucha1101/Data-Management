d99b2d99317e4d81617d7cc6955ae39462e67e5c
rucha1101 <rucha.khartadkar123@gmail.com>
2024-03-18 18:05:34 +0000

    FINAL CODE FOR VALIDATION

--- R/demo.R
+++ R/demo.R
@@ -25,32 +25,6 @@ append_csv_to_table <- function(file_path, connection) {
 }
 
 
-
-#workin......
-categories_path <- "data_upload/categories.csv"
-products_path <- "data_upload/products.csv"
-
-if(file.exists(categories_path) && file.exists(products_path)) {
-  categories_data <- read_csv(categories_path, show_col_types = FALSE)
-  products_data <- read_csv(products_path, show_col_types = FALSE)
-
-  # Using SQL query with sqldf
-  missing_product_ids <- sqldf("SELECT c.product_id
-                                FROM categories_data c
-                                LEFT JOIN products_data p ON c.product_id = p.product_id
-                                WHERE p.product_id IS NULL")
-
-  error_block <- if(nrow(missing_product_ids) > 0) {
-    stop(paste("There are product IDs in 'categories' that don't exist in 'products'. Data insertion halted."))
-  }
-  else {
-    message("Validation successful: All product IDs in 'categories' exist in 'products'")
-  
-  }
-}
-
-
-
 all_files <- list.files(path = "data_upload", pattern = "\\.csv$", full.names = TRUE)
 lapply(all_files, append_csv_to_table, my_connection)
 
@@ -77,26 +51,52 @@ display_table_counts <- function(connection) {
 display_table_counts(my_connection)
 
 
-# 
-# # 1. Top 10 Products
-# 
-# sql_query <- "
-# SELECT p.product_name, AVG(r.rating) AS average_rating
-# FROM reviews r
-# JOIN products p ON r.product_id = p.product_id
-# GROUP BY p.product_id
-# ORDER BY average_rating DESC
-# LIMIT 10
-# "
-# 
-# top_rated_products <- dbGetQuery(my_connection, sql_query)
-# 
-# # visualising
-# ggplot(top_rated_products, aes(x = reorder(product_name, average_rating), y = average_rating, fill = average_rating)) +
-#   geom_col() +
-#   coord_flip() +  # Horizontal bars for better readability
-#   labs(title = "Top 10 Rated Products", x = "Product Name", y = "Average Rating") +
-#   scale_fill_viridis_c() +  # Use a nice color scale for the fill
-#   theme_minimal()
-# 
+# VALIDATION CODE
+categories_path <- "data_upload/categories.csv"
+products_path <- "data_upload/products.csv"
+
+if(file.exists(categories_path) && file.exists(products_path)) {
+  categories_data <- read_csv(categories_path, show_col_types = FALSE)
+  products_data <- read_csv(products_path, show_col_types = FALSE)
+  
+  # Using SQL query with sqldf
+  missing_product_ids <- sqldf("SELECT c.product_id
+                                FROM categories_data c
+                                LEFT JOIN products_data p ON c.product_id = p.product_id
+                                WHERE p.product_id IS NULL")
+  
+  error_block <- if(nrow(missing_product_ids) > 0) {
+    stop(paste("There are product IDs in 'categories' that don't exist in 'products'. Data insertion halted."))
+  }
+  else {
+    message("VALIDATION SUCCESSFUL!!!: All product IDs in 'categories' exist in 'products'")
+    
+  }
+}
+
+
+
+
+
+# 1. Top 10 Products
+
+sql_query <- "
+SELECT p.product_name, AVG(r.rating) AS average_rating
+FROM reviews r
+JOIN products p ON r.product_id = p.product_id
+GROUP BY p.product_id
+ORDER BY average_rating DESC
+LIMIT 10
+"
+
+top_rated_products <- dbGetQuery(my_connection, sql_query)
+
+# visualising
+ggplot(top_rated_products, aes(x = reorder(product_name, average_rating), y = average_rating, fill = average_rating)) +
+  geom_col() +
+  coord_flip() +  # Horizontal bars for better readability
+  labs(title = "Top 10 Rated Products", x = "Product Name", y = "Average Rating") +
+  scale_fill_viridis_c() +  # Use a nice color scale for the fill
+  theme_minimal()
+
 dbDisconnect(my_connection)
